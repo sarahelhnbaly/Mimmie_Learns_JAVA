14e874808f3897ae9d9a1514a2384376d079f19e
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-06 14:17:42 +0200

    okay

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
+import com.sun.jdi.Value;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-        System.out.println("Welcome to Java!");
-
+        Test obj1 = new Test(15);
+        System.out.println(obj1.age);
+        Test obj2 = new Test(22);
+        System.out.println(obj2.age);
 
 
         }
+    }
+    class Test{
+        int age;
+        String message;
+
+        Test(){
+        System.out.println("I am a Constructor");
+    }
+    Test(int value){
+            this.age = value;
+            System.out.println("I am setting age " );
+
+    }
+    Test(int value, String message){
+            this.age = value;
+            this.message = message;
+            System.out.println("I am setting message, and age" + "Hi1 ");
+    }
+
+
+
     }
