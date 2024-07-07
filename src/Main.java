8faa45aeb83f248cbdf12268006b4de6e5f2bf9d
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-07 18:36:58 +0200

    okay

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
        public static void main(String[] args) {
-           Calculator calc=new Calculator();
-
-           int sumOFcalc= calc.add(10,8);
-           System.out.println(sumOFcalc);
-
-
-           Calculator calc1=new Calculator();
-
-           int sumOFcalc1= calc1.add(2,3);
-           System.out.println(sumOFcalc1);
+          Scanner obj = new Scanner(System.in);
+          System.out.println("Enter your age: ");
+          int age = obj.nextInt();
+          Integer ageObject = age;
+          int age1= ageObject.intValue();
+          System.out.println("your age is: "+ ageObject);
 
 
        }
