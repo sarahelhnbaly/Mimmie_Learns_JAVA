e5646400ce019638ae0356e5198f424e92de9d32
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-07 18:27:02 +0200

    okay

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,22 +1,13 @@
 public class Calculator {
-    int age;
-    String message;
+    int firstNumber, secondNumber;
+    public static int add(int value1, int value2 ) {
+        return value1 + value2;
+        //return;
 
-    Calculator() {
-        System.out.println("I am a Constructor");
-    }
 
-    Calculator(int value) {
-        this.age = value;
-        System.out.println("I am setting age ");
+
 
     }
-
-    Calculator(int value, String message) {
-        this.age = value;
-        this.message = message;
-        System.out.println("I am setting message, and age" + "Hi1 ");
     }
 
 
-}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        Calculator obj1 = new Calculator(15,"Hello World");
-        System.out.println(obj1.age);
-        System.out.println(obj1.message);
-        Calculator obj2 = new Calculator(22,"I LOVE YOU");
-        System.out.println(obj2.age);
-        System.out.println(obj2.message);
-        Calculator obj3 = new Calculator();
-        System.out.println(obj3.age);
-        System.out.println(obj3.message);
+
+       public static void main(String[] args) {
+           Calculator calc=new Calculator();
+
+           int sumOFcalc= calc.add(10,8);
+           System.out.println(sumOFcalc);
 
 
-        }
+           Calculator calc1=new Calculator();
+
+           int sumOFcalc1= calc1.add(2,3);
+           System.out.println(sumOFcalc1);
+
+
+       }
     }
