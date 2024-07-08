40bd461828fa2691a3efbb8e9b3a9dcc34cb4ca5
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-08 18:30:55 +0200

    okay

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
-import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
 
-       public static void main(String[] args) {
-          Scanner obj = new Scanner(System.in);
-          System.out.println("Enter your age: ");
-          int age = obj.nextInt();
-          Integer ageObject = age;
-          int age1= ageObject.intValue();
-          System.out.println("your age is: "+ ageObject);
-
-
-       }
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter day number");
+        int noOfDay = obj.nextInt();
+        String day = switch (noOfDay) {
+            case 1, 7 -> "Weekend, sleep well";
+            case 2, 3, 4, 5, 6 -> "Weekdays, do your BEST!";
+            default -> "Error - wrong input";
+        };
+        System.out.println(day);
     }
+}
