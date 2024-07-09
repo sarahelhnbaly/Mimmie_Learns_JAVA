4f4d4e9a8b65f4271e9cebfa4bb8d3eac29f4578
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-09 13:47:50 +0200

    okay

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
         String [] fruits = {"Apple", "Banana", "Orange", "Grape"};
         int index=0;
         int length=fruits.length;
+        boolean value= false;
+        do {
+            System.out.println("The value of the:"+ fruits[index]);
+            System.out.println("iam in the loop ");
+            index++;
 
-     while(length>index){
-         System.out.println("The value of the:"+ fruits[index]);
-         System.out.println("iam in the loop ");
-         index++;
+        }
 
-
-
-     }
+     while(length>index);
      System.out.println("iam out side the loop ");
 
     }
