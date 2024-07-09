ec7122f8d0af21a594a6fdffd69bec60defe329f
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-09 11:00:23 +0200

    okay

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.println("Enter day number");
-        int noOfDay = obj.nextInt();
-        String day = switch (noOfDay) {
-            case 1, 7 -> "Weekend, sleep well";
-            case 2, 3, 4, 5, 6 -> "Weekdays, do your BEST!";
-            default -> "Error - wrong input";
-        };
-        System.out.println(day);
+        String [] [] fruits = {{"Apple", "Banana", "Orange" }, {"Grape","Kiwi","Guava"}};
+        int [] weeknumbers = {1, 2, 3, 4, 5, 6, 7};
+        int len = fruits.length;
+        String fruit[]=fruits[0];
+
+        System.out.println(fruits[1][2]);
+        System.out.println(weeknumbers[6]);
+        System.out.println(fruits.length);
+        System.out.println(weeknumbers.length);
+
+
+
+
+
     }
 }
