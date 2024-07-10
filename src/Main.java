5b41817257d6c11da62d9c88db30af2db326d658
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-10 10:59:58 +0200

    okay

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String [] fruits = {"Apple", "Banana", "Orange", "Grape"};
-        int index=0;
-        int length=fruits.length;
-        boolean value= false;
-        do {
-            System.out.println("The value of the:"+ fruits[index]);
-            System.out.println("iam in the loop ");
-            index++;
+        String s =" ";
+        String name="Ashtosh";
+        System.out.println("s");
 
-        }
 
-     while(length>index);
-     System.out.println("iam out side the loop ");
 
     }
 }
