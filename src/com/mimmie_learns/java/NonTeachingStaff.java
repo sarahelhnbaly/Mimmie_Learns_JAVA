43f1de45682827074587ee77d304c0d7456d5641
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-17 03:47:46 +0200

    okay

---
+++ src/com/mimmie_learns/java/NonTeachingStaff.java
@@ -0,0 +1,13 @@
+public class NonTeachingStaff {
+    private String role;
+
+    // getter and setter for role
+    public void setRole(String role){
+        this.role = role;
+    }
+
+    public String getRole(){
+        return this.role;
+    }
+    // other methods...
+}
