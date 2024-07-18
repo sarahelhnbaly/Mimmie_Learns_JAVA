2b42dc7bb5ef46423c6b53209b172bd89a734fcb
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-18 20:40:13 +0200

    okay

--- src/com/mimmie_learns/java/Main.java
+++ src/com/mimmie_learns/java/Main.java
@@ -1,28 +1,16 @@
 package com.mimmie_learns.java;
 import com.mimmie_learns.java.abstracrexample.*;
 import com.mimmie_learns.java.interfaces.IShape;
+import com.mimmie_learns.java.pojo.Person;
 
 public class Main {
     public static void main(String[] args) {
-        IShape shape1 = new Circle(3.5);
-        Shape shape2 = new Rectangle(3.5, 3.5);
-        IShape shape22 = new Rectangle(3,5);
-        Shape shape3 = new Trapedzoid(3.0, 4.0, 6.5);
+        Person obj1 = new Person("x",22);
+        Person obj2=new Person("Y",23);
+        Person obj3=new Person("Z",24);
+        Person obj4=new Person("X",25);
+        System.out.println(obj1.equals(obj2));
 
-
-        ShapeUtility util = new ShapeUtility();
-        util.getArea(shape1);
-        shape1.printName();
-        shape22.printName();
-        shape1.printName();
-        util.getArea(shape22);
-        util.calculateArea(shape2);
-        System.out.println(shape1.toString());
-        util.calculateArea(shape3);
-
-        System.out.println("Circle area: " + shape1.getArea());
-        System.out.println("Rectangle area: " + shape2.calculateArea());
-        System.out.println("Trapezoid area: " + shape3.calculateArea());
     }
 }
 
--- src/com/mimmie_learns/java/abstracrexample/Circle.java
+++ src/com/mimmie_learns/java/abstracrexample/Circle.java
@@ -5,9 +5,17 @@ import com.mimmie_learns.java.interfaces.IShape;
 
 public class Circle implements IShape {
     private double radius;
+    @Override
+    public String toString() {
+        return "the radius of circle is " + radius;
+    }
+
 
     public Circle(double radius) {
         this.radius = radius;
+
+
+
     }
 
     @Override
