e951f3b8ffecb37f853b2d3c85c794d0714294e6
sarahelhnbaly <152740185+sarahelhnbaly@users.noreply.github.com>
2024-07-18 22:59:57 +0200

    inoutPut.java

--- inoutPut.java
+++ inoutPut.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 class AssignmentOperator {
     public static void main(String[] args) {
-    	
-        System.out.println("Java programming is interesting.");   
+        Scanner scanner = new Scanner(System.in);
+        
+        // Output
+        System.out.println("Enter your name:");
+        
+        // Input
+        String name = scanner.nextLine();
+        
+        // Output
+        System.out.println("Hello, " + name + "! Java programming is interesting.");
+        
+        scanner.close();
     }
 }
